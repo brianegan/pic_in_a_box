1b9c11128aa931caa8ced42246e0d92b738137be
brianegan <brian.egan@unlv.edu>
2010-03-30 15:41:54 -0700

    Adding the JSpec suite into the actual plugin with an updated version of the jquery spec suite to detect be_animated

--- spec/node.js
+++ spec/node.js
@@ -1,5 +1,5 @@
 
-require.paths.unshift('spec', '/usr/local/lib/ruby/gems/1.9.1/gems/jspec-4.1.0/lib', 'lib')
+require.paths.unshift('spec', './spec/lib', 'lib')
 require('jspec')
 require('unit/spec.helper')
 require('yourlib')
--- spec/rhino.js
+++ spec/rhino.js
@@ -1,6 +1,6 @@
 
-load('/usr/local/lib/ruby/gems/1.9.1/gems/jspec-4.1.0/lib/jspec.js')
-load('/usr/local/lib/ruby/gems/1.9.1/gems/jspec-4.1.0/lib/jspec.xhr.js')
+load('./spec/lib/jspec.js')
+load('./spec/lib/jspec.xhr.js')
 load('lib/yourlib.js')
 load('spec/unit/spec.helper.js')
 
